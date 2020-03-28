a3b127cda0d82cd1b2ee65a7360154b49de965ac
Murat ÖZTÜRK <muratozturk@Murat-MacBook-Air.local>
2020-03-28 14:50:39 +0300

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Murat ÖZTÜRK on 28.03.2020.
+//  Copyright © 2020 Murat ÖZTÜRK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
